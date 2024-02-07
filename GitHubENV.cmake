04e213149b043d6abe0bf0a70c7fb4636a105ef1
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2024-02-07 00:34:16 -0800

    feat: add pjuce_ci_write_env_file

--- GitHubENV.cmake
+++ GitHubENV.cmake
@@ -1,13 +1,27 @@
-# Write some temp files to make GitHub Actions / packaging easy
+include_guard()
 
-if ((DEFINED ENV{CI}))
-    set (env_file "${PROJECT_SOURCE_DIR}/.env")
-    message ("Writing ENV file for CI: ${env_file}")
+# Write .env file is written to the root of the project inside CI for easier packaging
+# It uses the PROJECT_NAME, PRODUCT_NAME, VERSION, BUNDLE_ID, and COMPANY_NAME.
+#
+# pjuce_ci_write_env_file(
+#   PRODUCT_NAME "Your Product Name"
+#   BUNDLE_ID "com.yourcompany.${PROJECT_NAME}"
+#   COMPANY_NAME "Your Company Name"
+# )
+#
+function(pjuce_ci_write_env_file)
+    set(_oneValueArgs PRODUCT_NAME BUNDLE_ID COMPANY_NAME)
+    cmake_parse_arguments(_PJ "" "${_oneValueArgs}" "" ${ARGN})
 
-    # the first call truncates, the rest append
-    file(WRITE  "${env_file}" "PROJECT_NAME=${PROJECT_NAME}\n")
-    file(APPEND "${env_file}" "PRODUCT_NAME=${PRODUCT_NAME}\n")
-    file(APPEND "${env_file}" "VERSION=${CURRENT_VERSION}\n")
-    file(APPEND "${env_file}" "BUNDLE_ID=${BUNDLE_ID}\n")
-    file(APPEND "${env_file}" "COMPANY_NAME=${COMPANY_NAME}\n")
-endif ()
+    if ((DEFINED ENV{CI}))
+        set (env_file "${PROJECT_SOURCE_DIR}/.env")
+        message ("Writing ENV file for CI: ${env_file}")
+
+        # the first call truncates, the rest append
+        file(WRITE  "${env_file}" "PROJECT_NAME=${PROJECT_NAME}\n")
+        file(APPEND "${env_file}" "PRODUCT_NAME=${_PJ_PRODUCT_NAME}\n")
+        file(APPEND "${env_file}" "VERSION=${PROJECT_VERSION}\n")
+        file(APPEND "${env_file}" "BUNDLE_ID=${_PJ_BUNDLE_ID}\n")
+        file(APPEND "${env_file}" "COMPANY_NAME=${_PJ_COMPANY_NAME}\n")
+    endif ()
+endfunction()
