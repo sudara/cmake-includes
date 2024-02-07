11ee4e17cede5317a7b3b78a2b3f46b93d6a80c0
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2024-02-07 00:39:14 -0800

    feat: add pjuce_link_ipp function

--- PamplejuceIPP.cmake
+++ PamplejuceIPP.cmake
@@ -1,11 +1,19 @@
-# When present, use Intel IPP for performance on Windows
-if (WIN32) # Can't use MSVC here, as it won't catch Clang on Windows
-    find_package(IPP)
-    if (IPP_FOUND)
-        target_link_libraries(SharedCode INTERFACE IPP::ipps IPP::ippcore IPP::ippi IPP::ippcv)
-        message("IPP LIBRARIES FOUND")
-        target_compile_definitions(SharedCode INTERFACE PAMPLEJUCE_IPP=1)
-    else ()
-        message("IPP LIBRARIES *NOT* FOUND")
+include_guard()
+
+# Add IPP support to a target
+#
+# pjuce_link_ipp(target)
+#
+function(pjuce_link_ipp target)
+    # When present, use Intel IPP for performance on Windows
+    if (WIN32) # Can't use MSVC here, as it won't catch Clang on Windows
+        find_package(IPP)
+        if (IPP_FOUND)
+            target_link_libraries(${target} INTERFACE IPP::ipps IPP::ippcore IPP::ippi IPP::ippcv)
+            message("IPP LIBRARIES FOUND")
+            target_compile_definitions(${target} INTERFACE PAMPLEJUCE_IPP=1)
+        else ()
+            message("IPP LIBRARIES *NOT* FOUND")
+        endif ()
     endif ()
-endif ()
+endfunction()
