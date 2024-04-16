526de6be43ef01a875c75fde0a8af20f65313947
Simon Conan <static.cats.audio@gmail.com>
2024-04-16 16:42:58 +0200

    Exclude .DS_Store from asset files.

--- Assets.cmake
+++ Assets.cmake
@@ -1,6 +1,7 @@
 # HEADS UP: Pamplejuce assumes anything you stick in the assets folder you want to included in your binary!
 # This makes life easy, but will bloat your binary needlessly if you include unused files
 file(GLOB_RECURSE AssetFiles CONFIGURE_DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/assets/*")
+list (FILTER AssetFiles EXCLUDE REGEX "/\\.DS_Store$") # We don't want the .DS_Store on macOS though...
 
 # Setup our binary data as a target called Assets
 juce_add_binary_data(Assets SOURCES ${AssetFiles})
