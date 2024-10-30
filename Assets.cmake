2035bfb6e9783ee2436b6336cbc24970a215ade0
gondre <charles@tritik.com>
2024-10-30 09:27:20 +0100

    fixed a cmake error when the 'Assets' folder is empty

--- Assets.cmake
+++ Assets.cmake
@@ -4,8 +4,13 @@ file(GLOB_RECURSE AssetFiles CONFIGURE_DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/asse
 list (FILTER AssetFiles EXCLUDE REGEX "/\\.DS_Store$") # We don't want the .DS_Store on macOS though...
 
 # Setup our binary data as a target called Assets
-juce_add_binary_data(Assets SOURCES ${AssetFiles})
+if (NOT AssetFiles STREQUAL "")
+    juce_add_binary_data(Assets SOURCES ${AssetFiles})
 
-# Required for Linux happiness:
-# See https://forum.juce.com/t/loading-pytorch-model-using-binarydata/39997/2
-set_target_properties(Assets PROPERTIES POSITION_INDEPENDENT_CODE TRUE)
+    # Required for Linux happiness:
+    # See https://forum.juce.com/t/loading-pytorch-model-using-binarydata/39997/2
+    set_target_properties(Assets PROPERTIES POSITION_INDEPENDENT_CODE TRUE)
+
+else()
+    message(STATUS "No assets found to add to the binary data")
+endif()
