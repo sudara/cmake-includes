cec227f9bf018ed2570a9e8203fd56efa6445a8f
Dave Thomas <7sharp9@mail.com>
2025-07-09 23:18:19 +0100

    Improve universal binary build logic for macOS
    Refines the conditions for building universal binaries by supporting both CI and FORCE_UNIVERSAL_BINARY environments, and properly handles multi-config generators by setting architectures for the Release configuration. This ensures more flexible and correct universal binary builds on macOS.

--- PamplejuceMacOS.cmake
+++ PamplejuceMacOS.cmake
@@ -6,9 +6,15 @@ set(CMAKE_OSX_DEPLOYMENT_TARGET "10.13" CACHE STRING "Support macOS down to High
 
 # Building universal binaries on macOS increases build time
 # This is set on CI but not during local dev
-if ((DEFINED ENV{CI}) AND (CMAKE_BUILD_TYPE STREQUAL "Release") AND NOT (CMAKE_SYSTEM_NAME STREQUAL "iOS"))
-    message("Building for Apple Silicon and x86_64")
-    set(CMAKE_OSX_ARCHITECTURES arm64 x86_64)
+if ((DEFINED ENV{CI} OR DEFINED FORCE_UNIVERSAL_BINARY) AND NOT (CMAKE_SYSTEM_NAME STREQUAL "iOS"))
+    # For multi-config generators, set architectures for Release config
+    if(CMAKE_CONFIGURATION_TYPES)
+        set(CMAKE_OSX_ARCHITECTURES_RELEASE "arm64;x86_64" CACHE STRING "Architecture for Release builds" FORCE)
+        message("Universal binary will be built for Release configuration")
+    elseif(CMAKE_BUILD_TYPE STREQUAL "Release")
+        message("Building for Apple Silicon and x86_64")
+        set(CMAKE_OSX_ARCHITECTURES arm64 x86_64)
+    endif()
 endif ()
 
 # By default we don't want Xcode schemes to be made for modules, etc
