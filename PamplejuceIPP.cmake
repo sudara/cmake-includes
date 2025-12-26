99e0e22c17c1e7d805dfe84be83d35e3fbeaf14d
Sudara <sudara@alonetone.com>
2025-12-26 19:28:50 +0100

    Download IPP for macOS and fix linking for macOS IPP builds

--- PamplejuceIPP.cmake
+++ PamplejuceIPP.cmake
@@ -5,13 +5,26 @@ if(WIN32)
     set(IPP_LIB "${IPP_ROOT}/build/native/win-x64/")
     set(IPP_LIBS ippsmt ippcoremt ippimt ippcvmt ippvmmt)
 elseif(APPLE)
-    # Enable IPP for Intel builds (not ARM), it's often faster than vdsp (on Intel)
+    # IPP is x86_64 only. For universal builds, we use -Xarch_x86_64 to conditionally
+    # compile with PAMPLEJUCE_IPP and link IPP libraries only for the x86_64 slice.
     if(CMAKE_OSX_ARCHITECTURES MATCHES "x86_64" OR
        (NOT CMAKE_OSX_ARCHITECTURES AND CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64"))
-        # Downloaded from pip: ipp-include and ipp-static wheels (version 2021.9.1)
-        set(IPP_ROOT "${CMAKE_SOURCE_DIR}/ipp-macos")
-        set(IPP_INC "${IPP_ROOT}/ipp_include-2021.9.1.data/data/include")
-        set(IPP_LIB "${IPP_ROOT}/ipp_static-2021.9.1.data/data/lib")
+        include(FetchContent)
+
+        # IPP wheels are just zip files, FetchContent can download and extract them
+        FetchContent_Declare(ipp_include
+            URL https://files.pythonhosted.org/packages/f0/a0/3b0329735b28f05cbb87ce86be09567a2ecdf9bcd92c50b05fd0a30b34b9/ipp_include-2021.9.1-py2.py3-none-macosx_10_15_x86_64.macosx_11_0_x86_64.whl
+            URL_HASH SHA256=5d01a621a9ca758180dffac9677d37ad23759e3c606a376c4bc85adc79a86e6c
+        )
+        FetchContent_Declare(ipp_static
+            URL https://files.pythonhosted.org/packages/fa/19/8b5d54390389b5a9910a111bc033cb0045dc2e1614745c2c98dc54061370/ipp_static-2021.9.1-py2.py3-none-macosx_10_15_x86_64.macosx_11_0_x86_64.whl
+            URL_HASH SHA256=d0e1f6643c1e1cf64722dd57b71aaa2692e40406abcecc3c3d575c519326390f
+        )
+        FetchContent_MakeAvailable(ipp_include ipp_static)
+
+        set(IPP_ROOT "${ipp_include_SOURCE_DIR}")
+        set(IPP_INC "${ipp_include_SOURCE_DIR}/ipp_include-2021.9.1.data/data/include")
+        set(IPP_LIB "${ipp_static_SOURCE_DIR}/ipp_static-2021.9.1.data/data/lib")
         set(IPP_MACOS TRUE)
     endif()
 elseif(UNIX)
@@ -28,15 +41,14 @@ if (DEFINED IPP_ROOT)
         target_include_directories(SharedCode INTERFACE "${IPP_INC}")
 
         if (IPP_MACOS)
-            # Use -Xarch_x86_64 to only apply IPP flags to x86_64 slice of universal binary
+            # For universal builds, use -Xarch_x86_64 to apply IPP flags only to x86_64 slice
             target_compile_options(SharedCode INTERFACE "SHELL:-Xarch_x86_64 -DPAMPLEJUCE_IPP=1")
+
+            # Link IPP libraries only for x86_64 slice of universal binary
+            # Use -Wl to pass library flags directly to linker, bypassing driver issues
+            # ippcore must come last as other libs depend on it
             target_link_options(SharedCode INTERFACE
-                "SHELL:-Xarch_x86_64 -L${IPP_LIB}"
-                "SHELL:-Xarch_x86_64 -lipps"
-                "SHELL:-Xarch_x86_64 -lippcore"
-                "SHELL:-Xarch_x86_64 -lippi"
-                "SHELL:-Xarch_x86_64 -lippcv"
-                "SHELL:-Xarch_x86_64 -lippvm"
+                "SHELL:-Xarch_x86_64 -Wl,-L${IPP_LIB},-lipps,-lippi,-lippcv,-lippvm,-lippcore"
             )
         else()
             target_link_directories(SharedCode INTERFACE "${IPP_LIB}")
